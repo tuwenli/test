7d1feb21b5e0de38e96b88d26c2d4d21653f5c51
tuwenli <33590074+tuwenli@users.noreply.github.com>
2021-05-06 14:08:33 +0800

    Update test11.py

--- test11.py
+++ test11.py
@@ -1,9 +1,22 @@
 
-import requests
-r = requests.get("https://www.klook.com") # klook首页
+import pytest
 
-r1 = requests.get('https://www.klook.com/v2/usrcsrv/component/page/home')
 
-#print(r.status_code)
-#print(r.text)
-print(r1)
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    '''case: test func'''
+    print("test_answer func")
+    assert func(3) == 4
+
+
+def test_demo():
+    '''case: test 'hello' in a '''
+    print("test_demo test 'hello' in a")
+    a = "hello world"
+    assert "hello" in a
+
+if __name__ == '__main__':
+    pytest.main(["-s", "test_demo.py"])
